d790f076b88ffa6dc6592f689b2b571f80e8f03f
legendary2303 <cgutierrezsalinas970@gmail.com>
2024-05-20 21:27:12 -0600

    feat(frontend) add new application

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import Welcome from '../Screen/Welcome/index.jsx'
+import '../Screen/Welcome/welcome.css'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Welcome />
+  </React.StrictMode>,
+)
